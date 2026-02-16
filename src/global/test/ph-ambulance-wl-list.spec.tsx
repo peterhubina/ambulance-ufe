5c5822397b92579f14aa96998042efd5ccc11e6c
peterhubina <peter.hubina1@gmail.com>
2026-02-16 11:40:23 +0100

    test

---
+++ src/global/test/ph-ambulance-wl-list.spec.tsx
@@ -0,0 +1,17 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { PhAmbulanceWlList } from '../../components/ph-ambulance-wl-list/ph-ambulance-wl-list';
+
+describe('ph-ambulance-wl-list', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [PhAmbulanceWlList],
+      html: `<ph-ambulance-wl-list></ph-ambulance-wl-list>`,
+    });
+    
+    const wlList = page.rootInstance as PhAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length
+
+    const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+    expect(items.length).toEqual(expectedPatients);
+  });
+});
